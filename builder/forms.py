93411becfa9829228d19a8287ed1b46c8b983481
Luan fonseca de farias <luanfonceca@gmail.com>
2013-03-26 15:44:22 -0300

    Change the form labels to pt-br and more user-friendly

--- builder/forms.py
+++ builder/forms.py
@@ -1,24 +1,64 @@
-from django.forms.fields import DateField, ChoiceField, MultipleChoiceField
-from django.forms.widgets import RadioSelect, CheckboxSelectMultiple
-from django.forms.extras.widgets import SelectDateWidget
-from django import forms
+#!/usr/bin/env python
+# encoding: utf-8
+
+from django.forms import *
 
 SAMPLE_CHOICES = [(x, x) for x in xrange(0, 5)]
 
-class TextFieldsForm(forms.Form):
-    text = forms.CharField()
-    integer = forms.IntegerField()
-    decimal = forms.DecimalField()
-    date = forms.CharField(widget=forms.DateInput(format='%Y-%m-%d'))
-    datetime = forms.CharField(widget=forms.DateTimeInput(format='%Y-%m-%d %H:%M:%S'))
-    textarea = forms.CharField(widget=forms.Textarea())
+class TextFieldsForm(Form):
+    text = CharField(
+        label=u"Campo Texto"
+    )
+    integer = IntegerField(
+        label=u"Campo Numérico"
+    )
+    decimal = DecimalField(
+        label=u"Campo Decimal",
+        help_text=u"Com duas (2) casas decimais"
+    )
+    date = DateField(
+        widget=DateInput(
+            format='%d/%m/%Y',
+        ),
+        label=u"Data",
+        help_text=u"formato: (dd/mm/aaaa)"
+    )
+    datetime = CharField(
+        widget=DateTimeInput(
+            format='%d/%m/%Y %H:%M:%S'
+        ),
+        label=u"Tempo",
+        help_text=u"formato: (dd/mm/aaaa hh:mm:ss)"
+    )
+    textarea = CharField(
+        widget=Textarea(),
+        label=u"Caixa de Texto",
+    )
 
 
-class MultipleChoiceFieldsForm(forms.Form):
-    select_multiple = forms.CharField(widget=forms.SelectMultiple(choices=SAMPLE_CHOICES))
-    checkbox = forms.CharField(widget=forms.CheckboxSelectMultiple(choices=SAMPLE_CHOICES))
+class MultipleChoiceFieldsForm(Form):
+    select_multiple = CharField(
+        widget=SelectMultiple(
+            choices=SAMPLE_CHOICES
+        ),
+        label=u"Multi-seleções",
+    )
+    checkbox = ChoiceField(
+        widget=CheckboxSelectMultiple(),
+        label=u"Multi-seleções",
+        choices=SAMPLE_CHOICES
+    )
 
 
-class SingleChoiceFieldsForm(forms.Form):
-    select = forms.CharField(widget=forms.Select(choices=SAMPLE_CHOICES))
-    radio = forms.ChoiceField(widget=forms.RadioSelect, choices=SAMPLE_CHOICES)
+class SingleChoiceFieldsForm(Form):
+    select = CharField(
+        widget=Select(
+            choices=SAMPLE_CHOICES
+        ),
+        label=u"Única Seleção"
+    )
+    radio = ChoiceField(
+        widget=RadioSelect(),
+        choices=SAMPLE_CHOICES,
+        label=u"Única Seleção"
+    )
